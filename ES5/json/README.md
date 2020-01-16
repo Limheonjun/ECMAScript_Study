4d3ae393d4ed83766ed376c69bb27e404cff0088
Limheonjun <jumong40@naver.com>
2020-01-16 16:40:03 +0900

    ES5 : json

---
+++ ES5/json/README.md
@@ -0,0 +1,8 @@
+# json
+
+## Decription
+- json을 native로 지원
+
+## Methods
+- JSON.parse() : JSON문자열을 Javascript객체로 변환
+- JSON.stringify() : Javascript객체를 JSON문자열로 변환
---
+++ ES5/json/json.js
@@ -0,0 +1,6 @@
+var obj = JSON.parse('{"name":"John"}');
+console.log(obj); // { name: 'John' }  => JSON OBject
+
+
+var obj2 = JSON.stringify(obj);
+console.log(obj2); // {"name":"John"}  => String
