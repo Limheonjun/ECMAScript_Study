c8233ca421cdc79780e225ac352dc008aa98b84f
Limheonjun <jumong40@naver.com>
2020-01-19 21:41:51 +0900

    ES7: exponentiation operator

---
+++ ES7/exponentiation operator/README.md
@@ -0,0 +1,4 @@
+# exponentiation operator
+
+## Description
+- x의 y제곱을 x**y로 표현할 수 있음
---
+++ ES7/exponentiation operator/app.js
@@ -0,0 +1,13 @@
+console.log(Math.pow(2,3)); // 8
+console.log(2**3); // 8
+
+let sqrt = 2;
+for(let i=1; i<=3; i++){
+    sqrt**=i;
+    console.log(sqrt); 
+}
+/*
+2
+4
+64
+*/
