690e15ab2a8832c3d3ba89a054f31a318f607df0
Limheonjun <jumong40@naver.com>
2020-01-19 20:25:49 +0900

    ES6: multi-line strings

---
+++ ES6/multi-line strings/README.md
@@ -0,0 +1,4 @@
+# multi-line strings
+
+## Description
+- 템플릿 리터럴을 사용하여 긴 문장을 간단하게 처리할 수 있음
---
+++ ES6/multi-line strings/multi-line_strings.js
@@ -0,0 +1,8 @@
+let sentence = `Once upon a time, there was a man who,
+has awesome shoes. He also handsome.`;
+
+console.log(sentence);
+/*
+Once upon a time, there was a man who,
+has awesome shoes. He also handsome.
+*/
