c08589e41f0cb7791473e0ada2814fe7c8a7f55a
Limheonjun <jumong40@naver.com>
2020-01-19 20:21:01 +0900

    ES6: template literals

---
+++ ES6/template literals/README.md
@@ -0,0 +1,4 @@
+# template literals
+
+## Description
+- 템플릿 리터럴을 제공하여 ${}를 사용하여 간단히 처리 가능
---
+++ ES6/template literals/template_literals.js
@@ -0,0 +1,11 @@
+let str = 'Javascript';
+
+// ES5
+var sentence1 = 'This is '+str;
+console.log(sentence1); // This is Javascript
+
+
+// ES6
+let sentence2 = `This is ${str}`;
+console.log(sentence2); // This is Javascript
+
