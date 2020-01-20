c098c5d745459555570f95bff50b6471b947bed7
Limheonjun <jumong40@naver.com>
2020-01-20 16:59:50 +0900

    ES10: trim

---
+++ ES10/string trim/README.md
@@ -0,0 +1,8 @@
+# trim
+
+## Description
+- 특정한 부분의 공백만 지우고 싶은 경우 사용
+
+## Features
+- trimStart와 trimLeft는 왼쪽, trimEnd와 trimRight는 오른쪽 공백을 지움
+- 같은 역할의 메소드가 2개인 이유는 하위호환성을 위해서 남겨둠
---
+++ ES10/string trim/trim.js
@@ -0,0 +1,14 @@
+const str = ' Hello ! ';
+
+console.log(str);
+console.log(str.trimStart());
+console.log(str.trimLeft());
+console.log(str.trimEnd());
+console.log(str.trimRight());
+/*
+ Hello !
+Hello !
+Hello !
+ Hello !
+ Hello !
+*/
